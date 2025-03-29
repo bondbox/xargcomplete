919a9d7c77c9a4ac699763e5cb42b822ba6b418b
Mingzhe Zou <zoumingzhe@outlook.com>
2025-03-29 23:10:10 +0800

    bump version to 0.1

--- xargcomplete/attribute.py
+++ xargcomplete/attribute.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 
 __project__ = "xargcomplete"
-__version__ = "0.1.alpha.1"
+__version__ = "0.1"
 __urlhome__ = "https://github.com/bondbox/xargcomplete/"
 __description__ = "Tab completion management"
 
